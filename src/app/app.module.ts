9490b4859235a82d59dd774790d5d186478dc73a
Mandeep Singh <munish4627@gmail.com>
2021-08-07 10:03:53 +0530

    feat(changes): 'local changes'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { JumbotronComponent } from './jumbotron/jumbotron.component';
+import { BootstrapFormComponent } from './bootstrap-form/bootstrap-form.component';
+import { BootstrapTableComponent } from './bootstrap-table/bootstrap-table.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JumbotronComponent,
+    BootstrapFormComponent,
+    BootstrapTableComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/bootstrap-form/bootstrap-form.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bootstrap-form',
+  templateUrl: './bootstrap-form.component.html',
+  styleUrls: ['./bootstrap-form.component.css'],
+})
+export class BootstrapFormComponent implements OnInit {
+  employee: { id: any; name: any; description: any; email: any } = {
+    id: null,
+    name: '',
+    description: '',
+    email: '',
+  };
+
+  constructor() {}
+  ngOnInit() {}
+
+  createEmployee() {
+    console.log('Employee created: ', this.employee);
+    this.employee = { id: null, name: '', description: '', email: '' };
+  }
+}
