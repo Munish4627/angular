e6d0dac9c85cc881552ed1c3cb0accb7158ce755
Munish4627 <munish4627@gmail.com>
2021-08-10 23:40:02 +0530

    feat(changes): 'changes'

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,15 +1,49 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder} from '@angular/forms';
+import { FormGroup } from '@angular/forms';
+import { FormControl } from '@angular/forms';
+import { Validators } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  formGroup: FormGroup | undefined;
+  constructor(private formBuilder: FormBuilder) { }
 
-  constructor() { }
+  ngOnInit() {
+    this.createForm();
+  }
 
-  ngOnInit(): void {
+  createForm() {
+    this.formGroup = this.formBuilder.group({
+      'username': ['', Validators.required],
+      'password': ['', Validators.required],
+    });
+  }
+
+
+  getError(el: any) {
+    switch (el) {
+      case 'user':
+        if (this.formGroup.get('username').hasError('required')) {
+          return 'Username required';
+        }
+        break;
+      case 'pass':
+        if (this.formGroup.get('password').hasError('required')) {
+          return 'Password required';
+        }
+        break;
+      default:
+        return '';
+    }
+  }
+
+  onSubmit(post: any) {
+    this.post = post;
+    console.log(post);
   }
 
 }
