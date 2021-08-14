323eb49df55a163e450ec7c5b9a0a6cceee08223
Munish4627 <munish4627@gmail.com>
2021-08-14 23:22:07 +0530

    feat(changes): 'changes'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,16 @@ import { AppService } from './app.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   smartphone: any = [];
   constructor(private api: AppService) {}
   title = 'angular';
+  firstName = 'Munish Kumar';
+
+  callme(event:any)
+  {
+    alert(event)
+  }
 }
--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { AppService } from 'src/app/app.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent implements OnInit, AfterViewInit {
   User: any = ['Super Admin', 'Author', 'Reader'];
-  constructor() { }
-
-  ngOnInit(): void {
+  @ViewChild('myDiv') myDivMunish: any;
+  constructor(private appService: AppService) {}
+  ngAfterViewInit(): void {
+    this.myDivMunish;
   }
-
+  uploadFile(event: any) {}
+  ngOnInit(): void {
+    this.appService.getCountries().subscribe((res) => {
+      console.log(res);
+    });
+  }
 }
