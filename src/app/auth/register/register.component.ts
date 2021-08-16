d2f5c00555f67908bb4b3689cfcf0d002e4ca7a5
Munish4627 <munish4627@gmail.com>
2021-08-16 09:31:34 +0530

    feat(changes): 'changes'

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { AppService } from 'src/app/app.service';
 
 @Component({
@@ -9,6 +9,40 @@ import { AppService } from 'src/app/app.service';
 export class RegisterComponent implements OnInit, AfterViewInit {
   User: any = ['Super Admin', 'Author', 'Reader'];
   @ViewChild('myDiv') myDivMunish: any;
+
+  @ViewChild('UploadFileInput') uploadFileInput: ElementRef | undefined;
+  myfilename = 'Select File';
+
+  // fileChangeEvent(fileInput: any) {
+
+  //   if (fileInput.target.files && fileInput.target.files[0]) {
+
+
+  //     this.myfilename = '';
+  //     Array.from(fileInput.target.files).forEach((file: File) => {
+  //       console.log(file);
+  //       this.myfilename += file.name + ',';
+  //     });
+
+  //     const reader = new FileReader();
+  //     reader.onload = (e: any) => {
+  //       const image = new Image();
+  //       image.src = e.target.result;
+  //       image.onload = rs => {
+
+  //         // Return Base64 Data URL
+  //         const imgBase64Path = e.target.result;
+
+  //       };
+  //     };
+  //     reader.readAsDataURL(fileInput.target.files[0]);
+
+  //     // Reset File Input to Selct Same file again
+  //     this.uploadFileInput.nativeElement.value = "";
+  //   } else {
+  //     this.myfilename = 'Select File';
+  //   }
+  // }
   constructor(private appService: AppService) {}
   ngAfterViewInit(): void {
     this.myDivMunish;
